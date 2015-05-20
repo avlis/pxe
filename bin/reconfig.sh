e8825141ca96f6dd44dc4dabbe335ddd6267b691
avlis <avlis.onurb@gmail.com>
2015-05-20 23:40:21 +0100

    bug on sending signal to dnsmasq

--- bin/reconfig.sh
+++ bin/reconfig.sh
@@ -2,4 +2,4 @@
 cd /cloudconfigserver/data
 /cloudconfigserver/bin/make_dnsmasq_dhcp_reservations.py > dnsmasq/dhcp_reservations
 /cloudconfigserver/bin/make_dnsmasq_dhcp_options.py > dnsmasq/dhcp_options
-killall5 -HUP dnsmasq
+kill -HUP $(pidof dnsmasq)
