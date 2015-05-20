070c9b695e9ff23668da28ad45b3df2f0cd26c54
avlis <avlis.onurb@gmail.com>
2015-05-20 23:24:01 +0100

    don't override pxe menus on every startup, just create if necessary; don't download coreos on background, safer first boot.

--- bin/startup.sh
+++ bin/startup.sh
@@ -4,21 +4,21 @@ echo Waiting for pipework to give us the eth1 interface...
 myIP=$(ip addr show dev eth1 | awk -F '[ /]+' '/global/ {print $3}')
 mySUBNET=$(echo $myIP | cut -d '.' -f 1,2,3)
 echo Got IP $myIP, on subnet $mySUBNET 
-echo configuring this instance...
+echo making sure the necessary data folders exist
 mkdir -p /cloudconfigserver/data/tftp/pxelinux.cfg
 mkdir -p /cloudconfigserver/data/dnsmasq
 cd /cloudconfigserver/data/tftp
 echo downloading pxe if needed
 [ ! -f pxelinux.0 ] && wget $MIRROR/debian/dists/$DIST/main/installer-$ARCH/current/images/netboot/debian-installer/$ARCH/pxelinux.0
 echo downloading coreos if needed
-[ ! -f coreos_stable_pxe.vmlinuz ] && wget http://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz -O coreos_stable_pxe.vmlinuz &
-[ ! -f coreos_stable_pxe_image.cpio.gz ] &&  wget http://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz -O coreos_stable_pxe_image.cpio.gz &
-[ ! -f coreos_beta_pxe.vmlinuz ] &&  wget http://beta.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz -O coreos_beta_pxe.vmlinuz &
-[ ! -f coreos_beta_pxe_image.cpio.gz ] &&  wget http://beta.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz -O coreos_beta_pxe_image.cpio.gz &
-echo making pxe menus
-printf "DEFAULT coreos-stable\nlabel coreos-stable\n\tmenu coreos\n\tkernel coreos_stable_pxe.vmlinuz\n\tappend initrd=coreos_stable_pxe_image.cpio.gz console=ttyS1,19200n8 console=tty0 coreos.autologin=ttyS1 coreos.autologin=tty0 cloud-config-url=http://$myIP:8080\n" >pxelinux.cfg/stable
-printf "DEFAULT coreos-beta\nlabel coreos-beta\n\tmenu coreos\n\tkernel coreos_beta_pxe.vmlinuz\n\tappend initrd=coreos_beta_pxe_image.cpio.gz console=ttyS1,19200n8 console=tty0 coreos.autologin=ttyS1 coreos.autologin=tty0 cloud-config-url=http://$myIP:8080\n" >pxelinux.cfg/beta
-echo making dnsmask config files from pxe_hosts.json file...
+[ ! -f coreos_stable_pxe.vmlinuz ] && wget http://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz -O coreos_stable_pxe.vmlinuz
+[ ! -f coreos_stable_pxe_image.cpio.gz ] &&  wget http://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz -O coreos_stable_pxe_image.cpio.gz
+[ ! -f coreos_beta_pxe.vmlinuz ] &&  wget http://beta.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz -O coreos_beta_pxe.vmlinuz
+[ ! -f coreos_beta_pxe_image.cpio.gz ] &&  wget http://beta.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz -O coreos_beta_pxe_image.cpio.gz
+echo making pxe menus if needed
+[ ! -f pxelinux.cfg/stable ] && printf "DEFAULT coreos-stable\nlabel coreos-stable\n\tmenu coreos\n\tkernel coreos_stable_pxe.vmlinuz\n\tappend initrd=coreos_stable_pxe_image.cpio.gz console=ttyS1,19200n8 console=tty0 coreos.autologin=ttyS1 coreos.autologin=tty0 cloud-config-url=http://$myIP:8080\n" >pxelinux.cfg/stable
+[ ! -f pxelinux.cfg/beta ] && printf "DEFAULT coreos-beta\nlabel coreos-beta\n\tmenu coreos\n\tkernel coreos_beta_pxe.vmlinuz\n\tappend initrd=coreos_beta_pxe_image.cpio.gz console=ttyS1,19200n8 console=tty0 coreos.autologin=ttyS1 coreos.autologin=tty0 cloud-config-url=http://$myIP:8080\n" >pxelinux.cfg/beta
+echo generating dnsmask config files from pxe_hosts.json file...
 cd /cloudconfigserver/data
 /cloudconfigserver/bin/make_dnsmasq_dhcp_reservations.py > dnsmasq/dhcp_reservations
 /cloudconfigserver/bin/make_dnsmasq_dhcp_options.py > dnsmasq/dhcp_options
