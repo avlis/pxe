225976c909e5c79c85a9068596a001cb356a8e85
avlis <avlis.onurb@gmail.com>
2015-05-20 13:31:10 +0100

    missing redirect

--- startup.sh
+++ startup.sh
@@ -12,8 +12,8 @@ echo downloading coreos if needed
 [ ! -f coreos_beta_pxe.vmlinuz ] &&  wget http://beta.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz -O coreos_beta_pxe.vmlinuz &
 [ ! -f coreos_beta_pxe_image.cpio.gz ] &&  wget http://beta.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz -O coreos_beta_pxe_image.cpio.gz &
 echo making pxe menus
-printf "DEFAULT coreos-stable\nlabel coreos-stable\n\tmenu default\n\tkernel coreos_stable_pxe.vmlinuz\n\tappend initrd=coreos_stable_pxe_image.cpio.gz console=ttyS1,19200n8 console=tty0 coreos.autologin=ttyS1 coreos.autologin=tty0 cloud-config-url=http://$myIP\n" pxelinux.cfg.stable
-printf "DEFAULT coreos-beta\nlabel coreos-beta\n\tmenu default\n\tkernel coreos_beta_pxe.vmlinuz\n\tappend initrd=coreos_beta_pxe_image.cpio.gz console=ttyS1,19200n8 console=tty0 coreos.autologin=ttyS1 coreos.autologin=tty0 cloud-config-url=http://$myIP\n" pxelinux.cfg.beta
+printf "DEFAULT coreos-stable\nlabel coreos-stable\n\tmenu default\n\tkernel coreos_stable_pxe.vmlinuz\n\tappend initrd=coreos_stable_pxe_image.cpio.gz console=ttyS1,19200n8 console=tty0 coreos.autologin=ttyS1 coreos.autologin=tty0 cloud-config-url=http://$myIP\n" >pxelinux.cfg.stable
+printf "DEFAULT coreos-beta\nlabel coreos-beta\n\tmenu default\n\tkernel coreos_beta_pxe.vmlinuz\n\tappend initrd=coreos_beta_pxe_image.cpio.gz console=ttyS1,19200n8 console=tty0 coreos.autologin=ttyS1 coreos.autologin=tty0 cloud-config-url=http://$myIP\n" >pxelinux.cfg.beta
 echo making dnsmask config files from pxe_hosts.json file...
 /cloudconfigserver/bin/make_dnsmasq_dhcp_reservations.py > dhcp_reservations.dnsmasq
 /cloudconfigserver/bin/make_dnsmasq_dhcp_options.py > dhcp_options.dnsmasq
