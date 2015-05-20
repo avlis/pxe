4bab2966f9229a2309a09121a46160dcf81fca36
avlis <avlis.onurb@gmail.com>
2015-05-20 14:55:51 +0100

    make sure tftp exists

--- startup.sh
+++ startup.sh
@@ -5,6 +5,7 @@ myIP=$(ip addr show dev eth1 | awk -F '[ /]+' '/global/ {print $3}')
 mySUBNET=$(echo $myIP | cut -d '.' -f 1,2,3)
 echo Got IP $myIP, on subnet $mySUBNET 
 echo configuring this instance...
+mkdir -p /cloudconfigserver/data/tftp/pxelinux.cfg
 cd /cloudconfigserver/data/tftp
 echo downloading pxe if needed
 [ ! -f pxelinux.0 ] && wget $MIRROR/debian/dists/$DIST/main/installer-$ARCH/current/images/netboot/debian-installer/$ARCH/pxelinux.0
@@ -14,7 +15,6 @@ echo downloading coreos if needed
 [ ! -f coreos_beta_pxe.vmlinuz ] &&  wget http://beta.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz -O coreos_beta_pxe.vmlinuz &
 [ ! -f coreos_beta_pxe_image.cpio.gz ] &&  wget http://beta.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz -O coreos_beta_pxe_image.cpio.gz &
 echo making pxe menus
-mkdir pxelinux.cfg
 printf "DEFAULT coreos-stable\nlabel coreos-stable\n\tmenu default\n\tkernel coreos_stable_pxe.vmlinuz\n\tappend initrd=coreos_stable_pxe_image.cpio.gz console=ttyS1,19200n8 console=tty0 coreos.autologin=ttyS1 coreos.autologin=tty0 cloud-config-url=http://$myIP\n" >pxelinux.cfg/stable
 printf "DEFAULT coreos-beta\nlabel coreos-beta\n\tmenu default\n\tkernel coreos_beta_pxe.vmlinuz\n\tappend initrd=coreos_beta_pxe_image.cpio.gz console=ttyS1,19200n8 console=tty0 coreos.autologin=ttyS1 coreos.autologin=tty0 cloud-config-url=http://$myIP\n" >pxelinux.cfg/beta
 echo making dnsmask config files from pxe_hosts.json file...
