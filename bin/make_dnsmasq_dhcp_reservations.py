13daabe16d93ad60f31ce91d872fedb9b045f02f
avlis <avlis.onurb@gmail.com>
2015-05-21 11:24:29 +0100

    hostname on pxe_hosts.json is now $hostname

--- bin/make_dnsmasq_dhcp_reservations.py
+++ bin/make_dnsmasq_dhcp_reservations.py
@@ -7,4 +7,4 @@ with open('pxe_hosts.json') as data_file:
     hosts_data = json.load(data_file)
 
 for hi in hosts_data['hosts']:
-	print "%s,set:%s,%s,%s" % (hosts_data['hosts'][hi]['macaddr'],hosts_data['hosts'][hi]['channel'],hi ,hosts_data['hosts'][hi]['hostname'])
+	print "%s,set:%s,%s,%s" % (hosts_data['hosts'][hi]['macaddr'],hosts_data['hosts'][hi]['channel'],hi ,hosts_data['hosts'][hi]['$hostname'])
